b1134352cddb513ca6b605d685724f9e05cb4b8b
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2019-11-15 12:33:25 +0200

    - add logging interceptor to retrofit client

--- app/src/main/java/com/jimmy/pagginglib_tutorial/networking/RetrofitClient.kt
+++ app/src/main/java/com/jimmy/pagginglib_tutorial/networking/RetrofitClient.kt
@@ -1,11 +1,13 @@
 package com.jimmy.pagginglib_tutorial.networking
 
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitClient {
-
         private var mInstance: Api? = null
         private const val BASE_URL = "https://api.stackexchange.com/2.2/"
 
@@ -18,9 +20,17 @@ object RetrofitClient {
         }
 
         private fun create (): Api {
+            Log.e(RetrofitClient::class.java.simpleName, "RetrofitClient created" )
 
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            
            return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(Api::class.java)
